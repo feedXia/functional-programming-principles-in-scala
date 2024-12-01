7a974198500194f552a570b3f85292d687727b57
xia <x.l@apple.com>
2024-12-01 21:46:48 +0000

    sqrt, 1.6 blocks & lexical scope, 1.7 tail rec

---
+++ src/main/scala/week1/Lecture 1.6 - Blocks and Lexical Scope.sc
@@ -0,0 +1,6 @@
+val x = 0
+def f(y: Int) = y + 1
+val y =
+  val x = f(3)
+  x * x
+val result = y + x
--- src/main/scala/week1/sqrt.scala
+++ src/main/scala/week1/sqrt.scala
@@ -4,15 +4,13 @@ import scala.annotation.tailrec
 def square(x: Double) = x * x
 def abs(x: Double) = if x > 0 then x else -x
 
-@tailrec
-def sqrtIter(guess: Double, x: Double): Double =
-  if isGoodEnough(guess, x) then guess
-  else sqrtIter(improve(guess, x), x)
-
-def improve(guess: Double, x: Double) = (guess + x / guess) / 2
-
-def isGoodEnough(guess: Double, x: Double) = abs(guess * guess - x) < 0.001
-
-def sqrt(x: Double) = sqrtIter(1.0, x)
+def sqrt(x: Double) =
+  @tailrec
+  def sqrtIter(guess: Double): Double =
+    if isGoodEnough(guess) then guess
+    else sqrtIter(improve(guess))
+  def improve(guess: Double) = (guess + x / guess) / 2
+  def isGoodEnough(guess: Double) = abs(guess * guess - x) < 0.001
+  sqrtIter(1.0)
 
 @main def test = println(sqrt(2))
